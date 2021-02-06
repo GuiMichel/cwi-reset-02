924c5722fc5a695a96ba7452a31b53afd59139aa
GuiMichel <guimichel.dev@gmail.com>
2021-02-06 15:01:19 -0300

    Alteração da Mensagem

--- meu-projeto/src/Main.java
+++ meu-projeto/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Reseters!");
+
+        System.out.println("Hello Reseters! Reset,Restart,Go!");
     }
 }
