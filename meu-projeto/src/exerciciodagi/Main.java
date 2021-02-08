1ca827c409db3bc3aa4db05f4e2556328cc8a237
GuiMichel <guimichel.dev@gmail.com>
2021-02-08 20:29:51 -0300

    adicionado os exercícios da Gi

---
+++ meu-projeto/src/exerciciodagi/Main.java
@@ -0,0 +1,10 @@
+package exerciciodagi;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Reseters!");//Alteração do Hello World
+        System.out.println("Multiplicação: "+ 5 + 2);
+        System.out.println("Resto da Divisão: "+5 % 2);
+        System.out.println("É maoir? "+ (5 > 2));
+    }
+}
