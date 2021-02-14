197d1e7152f81cb717d323d7f462cb35146f36ad
GuiMichel <guimichel.dev@gmail.com>
2021-02-14 17:52:57 -0300

    Atualização Exercícios Aula2 + minha casa minha desgraça

--- Aula-02/src/Exercicio1/App.java
+++ Aula-02/src/Exercicio1/App.java
@@ -5,13 +5,13 @@ public class App {
         Diretor pedro = new Diretor("Pedro",60,10,Genero.MASCULINO);
         Ator caprio = new Ator("Leonardo", 30, 10, Genero.MASCULINO);
 
-//        Filme filme1 = new Filme("Titanic", "Barco Afundando",120,
-//                2000,5,pedro);
-//        Filme filme2 = new Filme("vingadores", "Fim do Mundo",90,
-//                2018,5,pedro);
-//
-//        filme1.reproduzir();
-//        filme2.reproduzir();
+        Filme filme1 = new Filme("Titanic", "Barco Afundando",120,
+                2000,7,pedro);
+        Filme filme2 = new Filme("Batman vs Superman", "Fim do Mundo",90,
+                2018,5,pedro);
+
+        filme1.reproduzir();
+        filme2.reproduzir();
 
         pedro.imprimir();
         caprio.imprimir();
--- Aula-02/src/Exercicio1/Filme.java
+++ Aula-02/src/Exercicio1/Filme.java
@@ -10,22 +10,27 @@ public class Filme {
 
 
     public Filme(String nome, String descricao, double duracao, int anoLancamento, int avaliacao, Diretor diretor) {
+        defineAvaliacao(avaliacao,nome);
         this.nome = nome;
         this.descricao = descricao;
         this.duracao = duracao;
         this.anoLancamento = anoLancamento;
-        this.avaliacao = avaliacao;
         this.diretor = diretor;
     }
 
     public void reproduzir(){
         System.out.println("Filme: "+ this.nome+"\nDescrição: "+this.descricao+"\nDuração: "+this.duracao+
-                "min\nDirigido por: "+this.diretor.getNome()+"\n");
+                "min\nDirigido por: "+this.diretor.getNome()+"\navaliação: "+this.avaliacao+"\n");
     }
 
-    private void defineAvaliacao(int avaliacao){
-
-
+    private void defineAvaliacao(int avaliacao, String nome){
+        if ("O Clube da Luta".equals(nome)){
+            this.avaliacao = 5;
+        }else if ("Batman vs Superman".equals(nome)){
+            this.avaliacao = 1;
+        }else if(avaliacao<1 || avaliacao>5){
+            this.avaliacao = 3;
+        }
     }
 
 }
